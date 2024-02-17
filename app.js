532d87ba1c94b01943ca6ca7e3514eb0b7332273
ShaheerAliDanish <149046491+ShaheerAliDanish@users.noreply.github.com>
2024-02-17 11:57:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,35 @@
+var a = document.getElementById('main')
+var b = document.getElementById('inputdisplay')
+function getNewelm() {
+    // paragrapgh 
+    var para = document.createElement('p')
+    para.innerHTML = b.value
+    // j.s | css
+    para.style.color = 'white'
+    para.style.fontSize = '30px'
+    para.style.padding = '10px'
+    // Delete Button
+    var btn = document.createElement('button')
+    var btnText = document.createTextNode('Delete')
+    btn.appendChild (btnText)
+    btn.setAttribute('onclick' ,'deleteText(this)')
+    para.appendChild(btn)
+    a.appendChild(para)
+    // j.s | css
+    btn.style.backgroundColor = 'white'
+    btn.style.width = '100px'
+    btn.style.height = '50px'
+    btn.style.border = 'none'
+    btn.style.borderRadius = '30px'
+    btn.style.fontSize = '20px'
+    btn.style.margin = '25px'
+    btn.style.cursor = 'pointer'
+
+}
+function  deleteText(text) {
+  var btn = text.parentNode
+  btn.remove();
+} 
+function deleteAll (){
+    a.innerHTML = " ";
+}    
